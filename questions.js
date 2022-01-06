34ddd059874272cc218160001cf5036c4f19e8ef
Killian Chamberlain <killian7195@gmail.com>
2022-01-06 17:13:24 -0600

    added audio and started test functions

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 var questions = [
   
     {
-      title:"Who created JavaScript?",
+      title: "Who created JavaScript?",
       choices:["Brendan Eich", "Albert Einstein", "Sir Roger Penrose", "Terence McKenna"],
       answer: "Brendan Eich"
     },
@@ -11,8 +11,8 @@ var questions = [
       answer: "Mocha" 
     },
     {
-      title:"In JavaScript, console.log does which of the following?",
-      choices: ["prints the element in a JSON-like tree", "prints the element in an HTML-like tree", "Puts random consoles on logs in the woods", "Initializes Var functions in JavaScript"],
+      title: "In JavaScript, console.log does which of the following?",
+      choices: ["prints the element in a JSON-like tree", "prints the element in an HTML-like tree", "Puts random consoles on logs in the woods", "Initializes var functions in JavaScript"],
       answer: "prints the element in an HTML-like tree"
     },
     {
@@ -21,8 +21,7 @@ var questions = [
       answer: "To declare a variable and store information"
     },
     {
-      title:
-      "In JavaScript, console.dir does which of the following?",
+      title: "In JavaScript, console.dir does which of the following?",
       choices: ["Panic-srickenly screams DIR in public bathrooms", "prints the element in an HTML-like tree", "prints the element in a JSON-like tree", "Adds directories to your console.log"],
       answer: "prints the element in a JSON-like tree"
     }
--- script.js
+++ script.js
@@ -9,4 +9,36 @@ var feedbackEl = document.getElementById("feedback");
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
+var Rightans = new Audio("assets\sounds\correctanswer.m4a");
+var Wrongans = new Audio("assets\sounds\wronganswer.m4a");
+
+function startQuiz() {
+  // hide screen
+  var startScreen = document.getElementById("start-screen");
+  startScreen.setAttribute("class", "start hide");
+
+
+  questionsEl.setAttribute("class", " ");
+
+  //  timer
+  timerId = setInterval(function(){
+    clockTick();
+  }, 1000);
+  // show starting time
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+var timeRemaining = 75;
+
+function timerStart(){
+    var quizInterval = setInterval(function(){
+        timeRemaining--;
+        $(".timer").text("Time: " + timeRemaining)
+
+        if( timeRemaining <= 0 ){
+            clearInterval(quizInterval)
+        }
+    }, 1000)
+}
 
